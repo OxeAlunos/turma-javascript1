ee96247f762794b103ba412920d0d3d869a38415
Adilson <ibalunos1@gmail.com>
2025-06-26 17:38:05 -0300

    criei uma função para exibir dados

---
+++ Exibirdados.js
@@ -0,0 +1,9 @@
+// criar função que recebe um aluno e imprime o nome, a idade, a nota 1, a nota 2 e a nota 3.
+function imprimirAluno(aluno){
+    console.log(aluno.nome)
+    console.log(aluno.idade)
+    console.log(aluno.nota1)
+    console.log(aluno.nota2)
+    console.log(aluno.nota3)
+
+}
